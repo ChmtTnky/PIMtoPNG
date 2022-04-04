940be3ff24558020e1aa80f16ca1dd9a8709b90e
ChmtTnky <okinamatara16@outlook.com>
2022-04-04 15:36:19 -0700

    Update Program.cs

--- PIMtoBMP/Program.cs
+++ PIMtoBMP/Program.cs
@@ -7,7 +7,7 @@ string pimfile = Console.ReadLine();
 // check if file exists
 if (!File.Exists(pimfile))
 {
-    Console.WriteLine("Invalid Filename");
+    Console.WriteLine("Invalid Filename\nExiting Execution");
     return;
 }
 
@@ -38,34 +38,69 @@ for (int i = 0; i < color_count; i++)
     palette[i] = Color.FromArgb(alpha, red, green, blue);
 }
 
-// get pixel data as a bit array, rather than a byte array
-// this is needed due to variable bit depths for color index values
-bool[] bitdata = new bool[(data.Length - pixel_start) * 8];
-for (int i = pixel_start; i < data.Length; i++)
-{
-    for (int j = 0; j < 8; j++)
-    {
-        bitdata[((i - pixel_start) * 8) + j] = ((data[i] >> (7 - j)) % 2) == 1;
-    }
-}
-
-// set pixel data
 Bitmap pimbmp = new Bitmap(width, height);
-for (int h = 0; h < height; h++)
+switch (bit_depth)
 {
-    for (int w = 0; w < width; w++)
-    {
-        int bit_index = ((h * width) + w) * bit_depth;
-        int color_index = 0;
+    case 4:
+        {
+            bool[] bitdata = new bool[(data.Length - pixel_start) * 8];
+            for (int i = pixel_start; i < data.Length; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    bitdata[((i - pixel_start) * 8) + j] = ((data[i] >> (7 - j)) % 2) == 1;
+                }
+            }
 
-        for (int i = 0; i < bit_depth; i++)
-            color_index += (int)(Math.Pow(2, bit_depth - i - 1) * Convert.ToInt32(bitdata[bit_index + i]));
+            for (int h = 0; h < height; h++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+                    int bit_index = ((h * width) + w) * bit_depth;
+                    int color_index = 0;
 
-        if (bit_depth != 4)
-            pimbmp.SetPixel(w, h, palette[color_index]);
-        else
-            pimbmp.SetPixel(w + (2 * ((w + 1) % 2) - 1), h, palette[color_index]);
-    }
+                    for (int i = 0; i < bit_depth; i++)
+                        color_index += (int)(Math.Pow(2, bit_depth - i - 1) * Convert.ToInt32(bitdata[bit_index + i]));
+
+                    pimbmp.SetPixel(w + (2 * ((w + 1) % 2) - 1), h, palette[color_index]);
+                }
+            }
+            break;
+        }
+    case 8:
+        {
+            for (int h = 0; h < height; h++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+                    int color_index = data[pixel_start + (h * width) + w];
+                    pimbmp.SetPixel(w, h, palette[color_index]);
+                }
+            }
+            break;
+        }
+    case 32:
+        {
+            for (int h = 0; h < height; h++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+                    int red = data[pixel_start + (4 * ((h * width) + w))];
+                    int green = data[pixel_start + (4 * ((h * width) + w)) + 1];
+                    int blue = data[pixel_start + (4 * ((h * width) + w)) + 2];
+                    int alpha = data[pixel_start + (4 * ((h * width) + w)) + 3] * 2;
+                    if (alpha > 255)
+                        alpha = 255;
+                    pimbmp.SetPixel(w, h, Color.FromArgb(alpha, red, green, blue));
+                }
+            }
+            break;
+        }
+    default:
+        {
+            Console.WriteLine("Unknown Bit-Depth/Incorrect Data Type\nExiting Execution");
+            return;
+        }
 }
 
 // get output file name
@@ -75,3 +110,5 @@ string newfilename = pimfile.Remove(pimfile.Length - 4) + ".bmp";
 pimbmp.Save(newfilename);
 if (File.Exists(newfilename))
     Console.WriteLine("New Bitmap Saved as " + newfilename);
+else
+    Console.WriteLine("Could not create file\nExiting Execution");
